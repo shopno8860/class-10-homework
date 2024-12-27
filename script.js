640c7f80293e7824cea202683c2fc361170ae3d6
shopno8860 <shopno8860@gmail.com>
2024-12-27 09:33:14 +0600

    modify Jquery

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
     $("#submenu_one, #submenu_two").hide();
       
-      $("#categories").on("click", function (e) {
-        e.stopPropagation();
-        $("#submenu_two").hide(); 
-        $("#submenu_one").toggle();
-      });
+    $("#categories").click(function (e) {
+      e.stopPropagation();
+      $("#submenu_two").hide(); 
+      $("#submenu_one").toggle();
+    });
   
-      $("#p_details").on("click", function (e) {
-        e.stopPropagation();
-        $("#submenu_one").hide(); 
-        $("#submenu_two").toggle();
-      });
+    $("#p_details").click(function (e) {
+      e.stopPropagation();
+      $("#submenu_one").hide(); 
+      $("#submenu_two").toggle();
+    });
   
-      $(document).on("click", function () {
-        $("#submenu_one, #submenu_two").hide();
-      });
-      
-      $("#submenu_one, #submenu_two").on("click", function (e) {
-        e.stopPropagation();
-      });
+    $(document).click(function () {
+      $("#submenu_one, #submenu_two").hide();
+    });
+    
+    $("#submenu_one, #submenu_two").click(function (e) {
+      e.stopPropagation();
+    });
   });
